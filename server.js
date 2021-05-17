1ad9594f7586c9dc8701b04032aee2a76caea2b4
Joshua Pauline <joshpauline@gmail.com>
2021-05-17 11:01:08 +1000

    cors fix

---
+++ server.js
@@ -0,0 +1,48 @@
+const express = require('express');
+const axios = require('axios');
+
+//allowing cross site requests
+const cors = require('cors');
+
+const app = express();
+
+//intiate app to run on port 5000
+const port = 5000;
+
+app.use(cors());
+app.use(express.json());
+
+app.get('/location', (req, res) => {
+  const queryParams = {
+    q: req.query.postCode,
+    state: req.query.state,
+  };
+
+  var strUrl =
+    'https://digitalapi.auspost.com.au/postcode/search.json?' +
+    Object.keys(queryParams)
+      .filter(function (key) {
+        return queryParams[key];
+      })
+      .map(function (key) {
+        return key + '=' + queryParams[key];
+      })
+      .join('&');
+
+  axios
+    .get(strUrl, {
+      headers: {
+        'AUTH-KEY': '872608e3-4530-4c6a-a369-052accb03ca8',
+      },
+    })
+    .then((resp) => {
+      res.send(resp.data);
+    })
+    .catch((er) => {
+      res.send(er);
+    });
+});
+
+app.listen(port, () => {
+  console.log(`server is running on port: ${port}`);
+});
